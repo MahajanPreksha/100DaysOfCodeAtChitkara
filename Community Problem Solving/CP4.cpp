52d12899d683b7fb9aed7e94df9bffb4bd80857e
MahajanPreksha <121788863+MahajanPreksha@users.noreply.github.com>
2023-05-21 21:12:38 +0530

    Create CP4.cpp

---
+++ Community Problem Solving/CP4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	for(int i=1;i<=T;i++){
+	  int X;
+	  cin>>X;
+	  if(X>=80){
+	    cout<<"YES"<<endl;
+	  }
+	  else{
+	    cout<<"NO"<<endl;
+	  }
+	}
+	return 0;
+}
