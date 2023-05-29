58c3cc16c4767a8f9a4040e08512f23846dd2be0
Preksha Mahajan <121788863+MahajanPreksha@users.noreply.github.com>
2023-05-30 02:44:36 +0530

    Create helloWorld.cpp

---
+++ Day1/helloWorld.cpp
@@ -0,0 +1,6 @@
+#include<iostream> //directive processed by pre-processor: pre-processor directive; header file
+using namespace std; /*std::cout<<"output"*/
+int main(){ //main function:- return_type function_name(){//code}
+    cout<<"Hello World"; //semicolon for termination of statement
+    return 0; //stop execution; code after this line won't run
+}
