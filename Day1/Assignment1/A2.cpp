892e9277034b87ddab37fed32ed0bde5f060c5d2
Preksha Mahajan <121788863+MahajanPreksha@users.noreply.github.com>
2023-05-30 14:44:18 +0530

    Add files via upload

---
+++ Day1/Assignment1/A2.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"ASCII Value of 'U' is: "<<(int)'U'<<endl;
+    return 0;
+}
---
+++ Day1/Assignment1/A4.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter the number: ";
+    cin>>num;
+    cout<<"Cube of the number is: "<<num*num*num<<endl;
+    return 0;
+}
