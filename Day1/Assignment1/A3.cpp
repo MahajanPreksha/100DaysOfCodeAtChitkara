97807f7a14dc387135821f2fae541e2ef5e548c2
Preksha Mahajan <121788863+MahajanPreksha@users.noreply.github.com>
2023-05-30 14:55:00 +0530

    Add files via upload

---
+++ Day1/Assignment1/A3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int length, breadth, area;
+    cout<<"Enter length: ";
+    cin>>length;
+    cout<<"Enter breadth: ";
+    cin>>breadth;
+    area = length * breadth;
+    cout<<"Area of rectangle = "<<area;
+    return 0;
+}
---
+++ Day1/Assignment1/A5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<" The sizeof(char): "<<sizeof(char)<<" bytes"<<endl;
+    cout<<" The sizeof(short): " <<sizeof(short)<<" bytes"<<endl;
+    cout<<" The sizeof(int):" <<sizeof(int)<<" bytes"<<endl;
+    cout<<" The sizeof(long): "<<sizeof(long)<<" bytes"<<endl;
+    cout<<" The sizeof(long long): "<<sizeof(long long)<<" bytes"<<endl;
+    cout<<" The sizeof(float): "<<sizeof(float)<<" bytes"<<endl;
+    cout<<" The sizeof(double): "<<sizeof(double)<<" bytes"<<endl;
+    cout<<" The sizeof(long double): "<<sizeof(long double)<<" bytes"<<endl;
+    cout<<" The sizeof(bool): "<<sizeof(bool)<<" bytes"<<endl;
+    return 0;
+}
