a00c4d10f3c24e19e1743c346c653e45b907ad7c
Preksha Mahajan <121788863+MahajanPreksha@users.noreply.github.com>
2023-06-01 19:34:45 +0530

    Create spiralMatrix.cpp

---
+++ Day26/spiralMatrix.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void spiralOrder(vector<vector<int> >&matrix){
+    int left = 0, top = 0;
+    int right = matrix[0].size()-1;
+    int bottom = matrix.size()-1;
+    int direction = 0;
+    while(left<=right && top<=bottom){
+        //left to right
+        if(direction==0){
+            for(int col=left; col<=right; col++){
+                cout<<matrix[top][col]<<" ";
+            }
+            top++;
+        }
+        //top to bottom
+        else if(direction==1){
+            for(int row=top; row<=bottom; row++){
+                cout<<matrix[row][right]<<" ";
+            }
+            right--;
+        }
+        //right to left
+        else if(direction==2){
+            for(int col=right; col>=left; col--){
+                cout<<matrix[bottom][col]<<" ";
+            }
+            bottom--;
+        }
+        //bottom to top
+        else{
+            for(int row=bottom; row>=top; row--){
+                cout<<matrix[row][left]<<" ";
+            }
+            left++;
+        }
+        direction = (direction+1)%4;
+    }
+}
+
+int main(){
+    int n, m;
+    cin>>n>>m;
+    vector<vector<int> > matrix(n, vector<int> (m));
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin>>matrix[i][j];
+        }
+    }
+    spiralOrder(matrix);
+    return 0;
+}
