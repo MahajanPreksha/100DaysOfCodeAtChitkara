90c8e43a89e5bb4c657ec3ab3c6425b0780dab80
Preksha Mahajan <121788863+MahajanPreksha@users.noreply.github.com>
2023-06-11 18:38:07 +0530

    Create prefixSumIn2DArraysApproach2.cpp

---
+++ Day36/prefixSumIn2DArraysApproach2.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int rectangleSum(vector<vector<int> >&matrix, int l1, int r1, int l2, int r2){
+    int sum = 0;
+    for(int i=0; i<matrix.size(); i++){
+        for(int j=0; j<matrix[0].size(); j++){
+            matrix[i][j]+=matrix[i][j-1];
+        }
+    }
+    for(int i=l1; i<=l2; i++){
+        sum += matrix[i][r2] - (r1>=1? matrix[i][r1-1] : 0);
+    }
+    return sum;
+}
+int main(){
+    int n, m;
+    cin>>n>>m;
+    vector<vector<int> > matrix(n, vector<int> (m));
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin>>matrix[i][j];
+        }
+    }
+    int l1, r1, l2, r2;
+    cin>>l1>>r1>>l2>>r2;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    int sum = rectangleSum(matrix, l1, r1, l2, r2);
+    cout<<sum;
+    return 0;
+}
