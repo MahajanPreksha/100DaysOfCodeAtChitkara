6d700c5a0d7c39219f9b87eee9984c990dcb3457
Preksha Mahajan <121788863+MahajanPreksha@users.noreply.github.com>
2023-06-18 20:52:39 +0530

    Update CP32.cpp

--- Community Problem Solving/CP32.cpp
+++ Community Problem Solving/CP32.cpp
@@ -5,20 +5,20 @@ int main() {
 	int T;
 	cin>>T;
 	while(T--){
-	  int N, a = 0, b = 0;
+	  int N, count = 0;
 	  cin>>N;
 	  string S;
 	  cin>>S;
 	  for(int i=0; i<N; i++){
 	    if(S[i]=='0'){
-	      a++;
+	      count++;
 	    }
 	  }
-	  if(a==(N-a)){
+	  if(count==(N-count)){
 	    cout<<N<<endl;
 	  }
 	  else{
-	    cout<<min(a,N-a)*2+1<<endl;
+	    cout<<min(count,N-count)*2+1<<endl;
 	  }
 	}
 	return 0;
