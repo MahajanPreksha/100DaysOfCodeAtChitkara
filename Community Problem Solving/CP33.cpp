3016953153702c286e4ff498de4380337a6a7fe4
Preksha Mahajan <121788863+MahajanPreksha@users.noreply.github.com>
2023-06-19 17:51:08 +0530

    Create CP33.cpp

---
+++ Community Problem Solving/CP33.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	  int B;
+	  cin>>B;
+	  int res = ((B/2)*(B/2)-(B/2))/2;
+	  cout<<res<<endl;
+	}
+	return 0;
+}
