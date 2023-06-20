cb35d87988f6929c54feed203d17f46fe73bdc5e
Preksha Mahajan <121788863+MahajanPreksha@users.noreply.github.com>
2023-06-20 16:04:01 +0530

    Add files via upload

---
+++ Day10/secondMaxElementInArray.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int largestEleIndex(int array[], int size){
+    int max = INT_MIN;
+    int maxIndex = -1;
+    for(int i=0; i<size; i++){
+        if(max<array[i]){
+            max = array[i];
+            maxIndex = i;
+        }
+    }
+    return maxIndex;
+}
+int main(){
+    int size;
+    cin>>size;
+    int array[size];
+    for(int i=0; i<size; i++){
+        cin>>array[i];
+    }
+    int largestIndex = largestEleIndex(array, size);
+    array[largestIndex] = -2345;
+    int indexOfSecLargest = largestEleIndex(array, size);
+    cout<<array[indexOfSecLargest];
+    return 0;
+}
