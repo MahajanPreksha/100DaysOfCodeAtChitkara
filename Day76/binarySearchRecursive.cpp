54ac32ddac6ebde24d0b7e87c21ff9292a515810
Preksha Mahajan <121788863+MahajanPreksha@users.noreply.github.com>
2023-07-21 20:09:08 +0530

    Add files via upload

---
+++ Day76/binarySearchRecursive.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int binarySearch(vector<int> &v, int target, int low, int high){
+    int mid = (low + high)/2;
+    if(low>high){
+        return -1;
+    }
+    if(v[mid]==target){
+        return mid;
+    }
+    if(v[mid]<target){
+        return binarySearch(v, target, mid+1, high);
+    }
+    else{
+        return binarySearch(v, target, mid-1, high);
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0; i<n; i++){
+        cin>>v[i];
+    }
+    int target;
+    cin>>target;
+    int low = 0, high = n - 1;
+    int res = binarySearch(v, target, low, high);
+    cout<<res;
+    return 0;
+}
