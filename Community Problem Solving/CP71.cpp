d9ca1b9802ef7637fa582a64a5edfa95d7eeb50d
Preksha Mahajan <121788863+MahajanPreksha@users.noreply.github.com>
2023-07-27 07:04:36 +0530

    Update CP71.cpp

--- Community Problem Solving/CP71.cpp
+++ Community Problem Solving/CP71.cpp
@@ -1,31 +1,22 @@
-#include <iostream>
-#include<algorithm>
+#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
-
-int main() {
-	int T;
-	cin>>T;
-	while(T--){
-	  int N;
-	  cin>>N;
-	  int H[N];
-	  for(int i=0; i<N; i++){
-	    cin>>H[i];
-	  }
-	  sort(H, H+N);
-	  int ans = 0;
-	  if(N==1){
-	    cout<<H[0]<<endl;
-	    continue;
-	  }
-	  for(int i=0; i<N; i++){
-	    int j = i, count = 0;
-	    while(i<N && H[i]==H[j]){
-	      i++, count++;
-	    }
-	    ans = max(ans, H[j]+count-1);
-	  }
-	  cout<<ans<<endl;
-	}
-	return 0;
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+      int N;
+      cin>>N;
+      map<int, int> mp;
+      for(int i=0; i<N; i++){
+        cin >> arr[i];
+        mp[arr[i]]++;
+      }
+    int ans = 0;
+    for(auto i : mp){
+        ans = max(ans, i.first + i.second - 1);
+    }
+    cout<<ans<<endl;    
+    }
+    return 0;
 }
