a3846c74d97ac87141fb36c1c75b2018a5dead74
Preksha Mahajan <121788863+MahajanPreksha@users.noreply.github.com>
2023-07-28 19:56:56 +0530

    Update CP72.cpp

--- Community Problem Solving/CP72.cpp
+++ Community Problem Solving/CP72.cpp
@@ -5,17 +5,17 @@ int main() {
 	int T;
 	cin>>T;
 	while(T--){
-	  int D, L, R;
-	  cin>>D>>L>>R;
-	  if(D<L){
-	    cout<<"Too early"<<endl;
-	  }
-	  else if(D>R){
-	    cout<<"Too late"<<endl;
-	  }
-	  else{
-	    cout<<"Take second dose now"<<endl;
-	  }
+	    int D, L, R;
+	    cin>>D>>L>>R;
+	    if(D>R){
+	      cout<<"Too Late"<<endl;
+	    }
+	    else if(D<L){
+	      cout<<"Too Early"<<endl;
+	    }
+	    else{
+	      cout<<"Take second dose now"<<endl;
+	    }
 	}
 	return 0;
 }
